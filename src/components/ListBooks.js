800070df2149db1cb63320c5187ed7a201e7db43
Salih Ahmet <salihahmet.nuhoglu@gmail.com>
2023-01-08 22:55:29 +0300

    delete action added

--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -6,8 +6,9 @@ import { Loading } from "./Loading";
 const ListBooks = (props) => {
 
 
-    const [books, setBooks] = useState(null)
-    const [categories, setCategories] = useState(null)
+    const [books, setBooks] = useState(null);
+    const [categories, setCategories] = useState(null);
+    const [didUpdate, setDidUpdate] = useState(false);
 
     useEffect(() => {
         axios
@@ -19,16 +20,31 @@ const ListBooks = (props) => {
                     .then(resCat => {
                         setTimeout(() => {
                             setCategories(resCat.data);
-                        }, 5000);
+                        }, 1000);
 
                     })
                     .catch((err) => console.log("categories err", err));
             })
-            .catch(err => console.log("books err", err));
-    }, []);
+            .catch((err) => console.log("books err", err));
+    }, [didUpdate]);
+
+    const deleteBook = (id) => {
+        console.log(`http://localhost:3004/books/${id}`);
+        axios
+            .delete(`http://localhost:3004/books/${id}`)
+            .then((res) => {
+                console.log("delete res", res);
+                setDidUpdate(!didUpdate);
+            })
+            .catch((err) => console.log(err));
+    };
+
+
     if (books === null || categories === null) {
         return <Loading />;
     }
+
+
     return (
         <div className="container my-5">
             <div className="my-3 d-flex justify-content-end">
@@ -42,7 +58,8 @@ const ListBooks = (props) => {
                         <th scope="col">Book Name</th>
                         <th scope="col">Author</th>
                         <th scope="col">Category</th>
-                        <th scope="col">ISBN</th>
+                        <th className="text-center" scope="col">ISBN</th>
+                        <th className="text-center" scope="col">Action</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -55,7 +72,16 @@ const ListBooks = (props) => {
                                     <td>{book.name}</td>
                                     <td>{book.author}</td>
                                     <td>{category.name}</td>
-                                    <td>{book.isbn}</td>
+                                    <td className="text-center">{book.isbn === "" ? "-" : book.isbn}</td>
+                                    <td>
+                                        <div className="btn-group" role="group" >
+                                            <button type="button"
+                                                className="btn btn-outline-danger btn-sm"
+                                                onClick={() => deleteBook(book.id)}>
+                                                Delete
+                                            </button>
+                                        </div>
+                                    </td>
                                 </tr>
                             )
                         })
